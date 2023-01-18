305ee9a23e8f10fd8615170a9f851ad75febd460
jhyungkim <dnlwmxj135@gmail.com>
2023-01-18 16:41:40 +0900

    first upload

---
+++ NaverMovieNews.java
@@ -0,0 +1,13 @@
+package com.killingtime.dto;
+
+import lombok.Data;
+
+@Data
+public class NaverMovieNews {
+	
+	private String title;
+	private String link;
+	private String description;
+	private String pubDate;
+
+}
