6d8c4627eb650d08fbcf39eb9fb9dbc928462a40
jhyungkim <dnlwmxj135@gmail.com>
2023-02-01 18:13:05 +0900

    강의

---
+++ develop-together/src/main/java/com/demoweb/dto/ClassTagDto.java
@@ -0,0 +1,16 @@
+package com.demoweb.dto;
+
+import lombok.Data;
+
+@Data
+public class ClassTagDto {
+	
+	private int classTagNo;
+	private int tagNo;
+	private int classno;
+	private String classType;
+	
+	private String tagName;
+	
+}
+
--- develop-together/src/main/java/com/demoweb/mapper/ClassMapper.java
+++ develop-together/src/main/java/com/demoweb/mapper/ClassMapper.java
@@ -2,10 +2,14 @@ package com.demoweb.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import com.demoweb.dto.BoardTagDto;
 import com.demoweb.dto.ClassDto;
+import com.demoweb.dto.ClassTagDto;
 
 @Mapper
 public interface ClassMapper {
@@ -17,5 +21,16 @@ public interface ClassMapper {
 			"FROM class " +
 			"WHERE classno = #{ classno } AND deleted = '0' ")
 	ClassDto selectClassDetail(int classno);
+
+	
+	@Select("SELECT ct.classTagNo, ct.tagNo, ct.classno, ct.classType, t.tagName " +
+			"FROM tag t, classtag ct " +
+			"WHERE t.tagNo = ct.tagNo " +
+			"AND ct.classno = #{ classno } AND ct.classType = #{ classType } ")
+	List<ClassTagDto> selectClassTagByClassNo(@Param("classno") int classno, @Param("classType") String classType);
+	
+	@Delete("DELETE from class " +
+			"WHERE classno = #{classno} ")
+	void deleteClass(int classno);
 	
 }
