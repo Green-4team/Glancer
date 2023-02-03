c202ec9b3f05c4da893eb04ebdedf420df99b5ee
sbh1893 <sbh1893@gmail.com>
2023-02-03 17:04:35 +0900

    update

--- develop-together-react/src/components/header/AppHeaderDropdown.js
+++ develop-together-react/src/components/header/AppHeaderDropdown.js
@@ -24,12 +24,9 @@ import {
 } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
-import avatar8 from './../../assets/images/avatars/8.jpg'
-import { useNavigate } from 'react-router'
-
 const AppHeaderDropdown = ({onLogout, loginInfo}) => {
  
-  const navigate = useNavigate();
+  
 
   return (
     <CDropdown variant="nav-item">
