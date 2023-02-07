16b27fbce3493df1f358b9baea01c590fc55d21a
sbh1893 <sbh1893@gmail.com>
2023-02-07 15:15:31 +0900

    update

--- develop-together/src/main/java/com/demoweb/mapper/MemberMapper.java
+++ develop-together/src/main/java/com/demoweb/mapper/MemberMapper.java
@@ -16,17 +16,7 @@ import com.demoweb.dto.MemberDto;
 
 @Mapper // mapper interface 구현 객체 생성 자동으로 처리
 public interface MemberMapper {
-	
-//	@Insert("INSERT INTO member (memberid, passwd, email) " +
-//			"VALUES (#{ memberId }, #{ passwd }, #{ email })")
-//	void insertMember(MemberDto member);
-//	
-//	@Select("SELECT memberid, email, usertype, regdate, active " +
-//			"FROM member " +
-//			"WHERE memberid = #{ memberId } AND passwd = #{ passwd } AND active = '1' ")
-//	MemberDto selectMemberByIdAndPasswd(@Param("memberId")String memberId, @Param("passwd") String passwd);
-
-	
+		
 	@Insert("INSERT INTO member (memberid, password, membertype ) " +
 			"VALUES ( #{ memberId }, #{ password  }, #{ membertype } ) ")
 	void insertFreeLancerInfo(AllMemberRegisterDto freelancerregister);
