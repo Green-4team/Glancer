eb27b5ee1abc5a2e31ec5deb6421a68ce6039559
iwinegev <jornojovanahw@gmail.com>
2023-02-10 10:17:11 +0900

    item

--- develop-together-react/src/views/search/searchItem.js
+++ develop-together-react/src/views/search/searchItem.js
@@ -1,6 +1,74 @@
-const SearchItem = (searchKeyword, category) => {
+import { cilCheckCircle } from '@coreui/icons';
+import CIcon from '@coreui/icons-react';
+import { CButton, CNavLink } from '@coreui/react';
+import moment from 'moment/moment';
+import { BsChatLeftDots, BsEye } from 'react-icons/bs';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
+const HoverBlueBlock = styled.div`
+.hoverBlue:hover {color: #24a0ed;}
+`;
 
-} 
+const SearchItem = ({ result, loginInfo, onSetTopicNo }) => {
+//     const { 
+//         title, name, occupation, program1, program2, program3, program4, language1, language2, language3, introduce, carreeryear, rate 
+//     } = result;
 
-export default SearchItem;
+//     return (
+//     <>
+      
+//       <tr>
+//         <td colSpan={3}>
+//           <div style={{marginTop: 10, marginBottom: 10}}>
+//           <div style={{display: 'flex', justifyContent: 'space-between'}}>
+//             <div style={{display: 'inline-block', textAlign: 'right', marginLeft: 'auto'}}>
+//               {
+//                 // chosen === false ? 
+//                 <CIcon icon={cilCheckCircle} size="lg" /> :
+//                 <CIcon icon={cilCheckCircle} style={{color: '#24a0ed'}} size="lg" />
+//               }
+//             </div>
+//           </div>
+//           <HoverBlueBlock>
+//             <CNavLink to='/board/qna/detail' component={NavLink} style={{display: 'inline-block', marginRight: 10}} state={{ boardNo: boardNo, loginInfo: loginInfo }}>
+//               <div style={{fontWeight: 'bold', fontSize: 18, marginTop: 8, marginBottom: 8}} class="hoverBlue">{title}</div>
+//             </CNavLink>
+//           </HoverBlueBlock>
+//           <div style={{display: 'flex', justifyContent: 'space-between'}}>
+//             <div>
+
+//               <CButton color='light'
+//                        size='sm'
+//                        onClick={ () => {
+//                         onSetTopicNo({ topicNo: topicNo })
+//                        }}
+//                        style={{color: '#24a0ed',
+//                                paddingTop: 0,
+//                                paddingBottom: 0,
+//                                marginRight: 10,
+//                                display: 'inline-block'}}>{topicName}</CButton>
+//               {
+//                 tags.map((tag) => {
+//                   const { tagName, tagNo } = tag;
+//                   return  (
+//                             <CNavLink to='/board/qna/tagsearch' component={NavLink} style={{display: 'inline-block', marginRight: 10}} state={{ tagNo: tagNo, loginInfo: loginInfo }}>
+//                               <HoverBlueBlock>
+//                                 <div class="hoverBlue">#{ tagName }</div>
+//                               </HoverBlueBlock>
+//                             </CNavLink>
+//                           )
+//                 })
+//               }
+//             </div>
+//             <div style={{fontSize: 16}}><BsEye />&nbsp;{views}&nbsp;&nbsp;&nbsp;&nbsp;<BsChatLeftDots />&nbsp;{commentCount}</div>
+            
+//           </div>
+//           </div>
+//         </td>
+//       </tr>
+//     </>
+//   )
+}
+
+export default SearchItem
